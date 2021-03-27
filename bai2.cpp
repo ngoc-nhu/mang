352323f989b25d77f941ae7b31f7d0aeb2a67959
ngoc-nhu <80908216+ngoc-nhu@users.noreply.github.com>
2021-03-27 21:40:45 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+#define MAX 100
+
+void nhap(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << " nhap phan tu thu a[" << i << "]=";
+		cin >> a[i];
+	}
+}
+void xuat(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << " phan tu thu " << i << " = ";
+		cout << a[i] << endl;
+	}
+}
+void tim_so_le(int a[], int n) {
+	int tam = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] % 2 != 0) {
+			tam = a[i];
+		}
+	}
+	cout << tam;
+}
+int main() {
+	int a[MAX];
+	int n;
+	do {
+		cout << " nhap so luong phan tu n = ";
+		cin >> n;
+	} while (n <= 0 || n > MAX);
+	nhap(a, n);
+	xuat(a, n);
+	tim_so_le(a, n);
+}
